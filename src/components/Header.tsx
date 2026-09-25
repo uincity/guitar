95db5c29562951e0c288091257ea689bbdc55878
uincity <uuincity@naver.com>
2026-09-25 15:23:05 +0900

    제목및 코드버튼 색 보정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <header className="site-header">
       <div className="brand-mark" aria-hidden="true"><Guitar size={22} /></div>
       <div>
-        <h1>Guitar Chord Player</h1>
+        <h1>Guitar Chord Player for Cathrine</h1>
         <p>코드를 보고, 누르고, 들어보세요.</p>
       </div>
     </header>
